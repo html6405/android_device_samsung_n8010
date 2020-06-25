ce4f0f9aa5b56cea72606ecc464bec3046fe095b
html6405 <peter.schelchshorn@zell-net.at>
2020-06-25 16:09:22 +0200

    Init device tree for the N8010 and N8013.

---
+++ lineage.mk
@@ -0,0 +1,26 @@
+# Release name
+PRODUCT_RELEASE_NAME := n8010
+
+# Inherit device configuration
+$(call inherit-product, device/samsung/n80xx-common/n80xx-common.mk)
+$(call inherit-product, device/samsung/n8010/n8010.mk)
+
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+
+# Inherit some common stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := n8010
+PRODUCT_NAME := lineage_n8010
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := GT-N8010
+PRODUCT_MANUFACTURER := samsung
+
+# Set build fingerprint / ID / Product Name ect.
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=p4notewifiue \
+	TARGET_DEVICE=p4notewifi \
+	BUILD_FINGERPRINT="samsung/p4notewifixx/p4notewifiww:4.4.2/KOT49H/N8010XXUDNE4:user/release-keys" \
+	PRIVATE_BUILD_DESC="p4notewifixx-user 4.4.2 KOT49H N8010XXUDNE4 release-keys"
---
+++ n8010.mk
@@ -0,0 +1,27 @@
+#
+# Copyright (C) 2012 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+$(call inherit-product, device/samsung/n80xx-common/n80xx-common.mk)
+
+LOCAL_PATH := device/samsung/n8010
+
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+
+# Rootdir
+PRODUCT_COPY_FILES += \
+    device/samsung/n8010/rootdir/init.target.rc:root/init.target.rc
+
+$(call inherit-product-if-exists, vendor/samsung/n8010/n8010-vendor.mk)
